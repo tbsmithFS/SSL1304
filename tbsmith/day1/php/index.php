b1f42664a698f453f6becd92e3aa79278d7d7e9b
tbsmithFS <tbsmith@fullsail.com>
2013-03-23 17:57:32 -0400

    Made initial files for day 1

---
+++ tbsmith/day1/php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+#include 'settings.php';
+
+header("Content-type: text/html");
+
+
+echo "hi";
+
+
+
+?>
---
+++ tbsmith/day1/php/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+// no trailing slashes
+
+$dayNum = 1;
+
+if (preg_match("toddbsmith.com", $_SERVER["SERVER_NAME"]) { //on the pi
+
+	define("WD", "/var/www/SSL1304/tbsmith/day$dayNum/php");
+
+} else { // localhost
+
+	define("WD", "/Users/tbsmith/FullSail/SSL1304/tbsmith/day$dayNum/php");
+
+}
+
+
+
+?>
+
