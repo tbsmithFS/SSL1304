c1d0d1517dd3dfe781d17a71424cf45cbabfe3ba
bsang <biakhleisang@gmail.com>
2013-04-12 17:29:37 -0400

    add screencast video link

---
+++ tbsmith/python.ssl1304.com/views/navbar.py
@@ -0,0 +1,20 @@
+<div class="navBar">
+
+<div class="navBarRow">
+    <a href=""><div class="navButton">Home</div></a>
+    <a href=""><div class="navButton">Top News</div></a>
+</div>
+
+<div class="navBarRow">
+    <a href=""><div class="navButton">Politics</div></a>
+    <a href=""><div class="navButton">Tech</div></a>
+</div>
+
+<div class="navBarRow">
+    <a href=""><div class="navButton">Sports</div></a>
+    <a href=""><div class="navButton" id="loginLink">Login</div></a>
+</div>
+
+</div>
+
+
