fe9734ca8672600895bd9b9747470e0a9ff2f273
Todd Smith <tbsmith@fullsail.com>
2013-04-17 18:11:02 -0400

    Removed dirs from my user directory

--- tbsmith/python.ssl1304.com 2/views/errors.py
+++
@@ -1,7 +0,0 @@
-<div id="bodyLeft">
-    This is errors body - left
-</div>
-
-<div id="bodyRight">
-    This is errors body - right
-</div>
--- tbsmith/python.ssl1304.com 3/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from models.DBConnector import DBConnector
-
-print "Content-type: text/html\n\n"
-
-result = DBConnector().get_random_user()
-
-print result
